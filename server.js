45e366472751945411dd1ef195397fb15bd905c5
deizeshot <deizeshot@gmail.com>
2023-08-24 12:44:09 +0000

    5151

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!\n');
+  // Redirect to the specified URL
+  res.writeHead(302, { 'Location': 'https://swkgstudio.github.io/' });
+  res.end();
 });
 
 const PORT = process.env.PORT || 3000;
